6660383e1ffd2a898ff98b2c3ff0dc090537dc1b
laaouatni <laaouatni.anas@outlook.it>
2022-03-18 21:26:49 +0100

    beta with design

---
+++ docs/functionalities/pages/PageSections/programma/programmaDATA.js
@@ -0,0 +1,20 @@
+let eventoContainerTemplate = document.querySelectorAll(".programma-template")[0];
+const url = "https://sheetdb.io/api/v1/ugfngdyqadjky";
+fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((dataJSON) => {
+        dataJSON.forEach((item, index) => {
+            let eventoContainer = eventoContainerTemplate.content.cloneNode(true);
+
+            let orarioSpan = eventoContainer.querySelectorAll(".orario-evento-programma")[0];
+            let descrizioneSpan = eventoContainer.querySelectorAll(".descrizione-evento-programma")[0];
+
+            orarioSpan.textContent = dataJSON[index].orario;
+            descrizioneSpan.textContent = dataJSON[index].evento;
+
+            document.getElementById("programma-table-container").appendChild(eventoContainer);
+
+        })
+    });

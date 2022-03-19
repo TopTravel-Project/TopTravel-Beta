a9ba1f477d9f45757a47d88bda8c1ac7de00215e
laaouatni <laaouatni.anas@outlook.it>
2022-03-19 20:22:32 +0100

    beta quasi funzionante

--- docs/functionalities/pages/PageSections/programma/programma.js
+++ docs/functionalities/pages/PageSections/programma/programma.js
@@ -1,15 +1,14 @@
 let dettagliBtn = document.querySelectorAll('.evento-info-button');
 let popupEvento = document.querySelector('#more-info-evento');
+let popupCheckBox = document.querySelector('#info-checkbox-btn');
 
-dettagliBtn.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        if (checkBox.checked) {
-            popupEvento.classList.remove('popup-close');
-            popupEvento.classList.add('popup-open');
-        } else {
-            popupEvento.classList.remove('popup-open');
-            popupEvento.classList.add('popup-close');
 
-        }
-    });
+popupCheckBox.addEventListener('change', (item) => {
+    if (popupCheckBox.checked) {
+        popupEvento.classList.remove('popup-close');
+        popupEvento.classList.add('popup-open');
+    } else {
+        popupEvento.classList.remove('popup-open');
+        popupEvento.classList.add('popup-close');
+    }
 });

c386a19545333e4a2dfcd19269eee63f1dbf7573
laaouatni <laaouatni.anas@outlook.it>
2022-03-19 20:03:48 +0100

    ho cambiato servizio che trasforma excel in json

--- docs/functionalities/pages/PageSections/programma/programmaDATA.js
+++ docs/functionalities/pages/PageSections/programma/programmaDATA.js
@@ -1,5 +1,5 @@
 let eventoContainerTemplate = document.querySelectorAll(".programma-template")[0];
-const url = "https://sheetdb.io/api/v1/ugfngdyqadjky";
+/* const url = "https://sheetdb.io/api/v1/ugfngdyqadjky";
 fetch(url)
     .then((response) => {
         return response.json();
@@ -16,4 +16,23 @@ fetch(url)
 
             document.getElementById("programma-table-container").appendChild(eventoContainer);
         })
+    }); */
+
+const url = "https://gsx2json.com/api?id=14uIKI2cUcjHNnQYxGMLArBByRnJIImOps-kPk6ugZNw&sheet=Foglio1";
+fetch(url)
+    .then((response) => {
+        return response.json();
+    })
+    .then((dataJSON) => {
+        dataJSON.columns.orario.forEach((item, index) => {
+            let eventoContainer = eventoContainerTemplate.content.cloneNode(true);
+
+            let orarioSpan = eventoContainer.querySelectorAll(".orario-evento-programma")[0];
+            let descrizioneSpan = eventoContainer.querySelectorAll(".descrizione-evento-programma")[0];
+
+            orarioSpan.textContent = dataJSON.columns.orario[index];
+            descrizioneSpan.textContent = dataJSON.columns.evento[index];
+
+            document.getElementById("programma-table-container").appendChild(eventoContainer);
+        });
     });

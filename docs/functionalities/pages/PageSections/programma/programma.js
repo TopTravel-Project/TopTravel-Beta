f33b6b9a0f5d1291dbf2e6aaa36c7ac55d51934e
laaouatni <laaouatni.anas@outlook.it>
2022-03-20 22:36:31 +0100

    BUG FIXED popup is show and removed onclick

--- docs/functionalities/pages/PageSections/programma/programma.js
+++ docs/functionalities/pages/PageSections/programma/programma.js
@@ -5,14 +5,13 @@ let popupCheckBox = document.querySelector('#info-checkbox-btn');
 let overlayPopup = document.querySelector('#overlay-popup-evento');
 
 popupCheckBox.addEventListener('change', (item) => {
-    let removePopup = null;
     if (popupCheckBox.checked) {
         popupEvento.classList.remove('popup-close');
         popupEvento.classList.add('popup-open');
         overlayPopup.classList.remove('popup-close');
         overlayPopup.classList.add('popup-open');
 
-        removePopup = window.addEventListener('click', (e) => {
+        window.addEventListener('click', function removePopup(e) {
             if (popupCheckBox.checked) {
                 if (!popupEvento.contains(e.target)) {
                     popupEvento.classList.remove('popup-open');
@@ -20,7 +19,9 @@ popupCheckBox.addEventListener('change', (item) => {
                     overlayPopup.classList.remove('popup-open');
                     overlayPopup.classList.add('popup-close');
                     popupCheckBox.checked = false;
-                    console.log("false popup")
+                    console.log("false popup");
+
+                    window.removeEventListener('click', removePopup);
                 }
             }
         });
@@ -29,7 +30,6 @@ popupCheckBox.addEventListener('change', (item) => {
         popupEvento.classList.add('popup-close');
         overlayPopup.classList.remove('popup-open');
         overlayPopup.classList.add('popup-close');
-        /* desactive eventlistener */
-        removePopup = null;
+
     }
 });

c56462befcfe3b79d5509378509979476e473184
laaouatni <laaouatni.anas@outlook.it>
2022-03-28 17:57:31 +0200

    mappa si autoadatta ed responsive

--- docs/functionalities/pages/PageSections/mappa/mappa.js
+++ docs/functionalities/pages/PageSections/mappa/mappa.js
@@ -4,6 +4,11 @@ let mappaContainer = document.querySelector("#mappa-container");
 let ServiziHeight = serviziPosizioneContainer.offsetHeight;
 mappaContainer.style.setProperty("--servizi-height", ServiziHeight + "px");
 
+window.addEventListener("load", () => {
+    ServiziHeight = serviziPosizioneContainer.offsetHeight;
+    mappaContainer.style.setProperty("--servizi-height", ServiziHeight + "px");
+});
+
 window.addEventListener("resize", () => {
     ServiziHeight = serviziPosizioneContainer.offsetHeight;
     mappaContainer.style.setProperty("--servizi-height", ServiziHeight + "px");
